f0de48e047f9663d9656f1fca1943f2dd8c61b67
aaronmichael726 <aaronmichael726@gmail.com>
2021-03-08 16:11:22 -0600

    fixed import statments

--- backend/models/commentsSchema.js
+++ backend/models/commentsSchema.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const commentsSchema = new Schema({
     user: String,
--- backend/models/hostSchema.js
+++ backend/models/hostSchema.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
-import commentsSchema from './commentsSchema'
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const commentsSchema = require('./commentsSchema')
 
 const hostSchema = new Schema({
     location: String,
