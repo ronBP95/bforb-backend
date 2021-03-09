7beefef565611972b9ec5a5aa460ee182786423c
unknown <erik@computer.com>
2021-03-09 09:30:23 -0700

    resolved conflicts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,5 @@
 module.exports = {
     places: require('./places'),
-<<<<<<< HEAD
-    profiles: require('./profiles')
-=======
+    profiles: require('./profiles'),
     comments: require('./comments')
->>>>>>> 064f6ea6ce2ba8b36fa6309f58771e1b1a8f3ecc
 }
